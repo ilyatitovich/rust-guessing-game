a2b876faa1944904c17ce3cafb3900b2c8ca26e8
ilyatitovich <ilyatitovich@gmail.com>
2024-12-29 13:48:38 +0100

    Add rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 // Standart library for input/output
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
-    // mut - means that varaible mutable and can change in the future
-    // String::new() - means that varaible store string
     let mut guess = String::new();
 
     io::stdin()
-        .read_line(&mut guess)  // handling user's input, using pointer
-        .expect("Failed to read line"); // handlign Errors
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    println!("You guessed: {guess}");
+    
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
